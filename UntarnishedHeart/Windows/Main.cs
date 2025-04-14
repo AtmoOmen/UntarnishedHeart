0589ba694b62c06b0638353761d9b14e5f1fbcd3
AtmoOmen <atmoomen2004@outlook.com>
2025-04-14 20:30:30 +0800

    主页拆分

--- UntarnishedHeart/Windows/Main.cs
+++ UntarnishedHeart/Windows/Main.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Dalamud.Game.ClientState.Objects.Enums;
+using Dalamud.Game.ClientState.Objects.Types;
 using Dalamud.Interface;
 using Dalamud.Interface.Colors;
 using Dalamud.Interface.Utility;
@@ -14,8 +15,7 @@ using UntarnishedHeart.Executor;
 
 namespace UntarnishedHeart.Windows;
 
-public class Main() : Window($"{PluginName} {Plugin.Version}###{PluginName}-MainWindow", 
-                             ImGuiWindowFlags.NoScrollbar), IDisposable
+public class Main() : Window($"{PluginName} {Plugin.Version}###{PluginName}-MainWindow"), IDisposable
 {
     public static Executor.Executor? PresetExecutor { get; private set; }
 
@@ -45,44 +45,66 @@ public class Main() : Window($"{PluginName} {Plugin.Version}###{PluginName}-Main
             Service.Config.Save();
         }
 
-        DrawExecutorInfo();
-
-        ImGui.Separator();
-        ImGui.Spacing();
-
-        DrawNecessaryInfo();
-
-        ImGui.Separator();
-        ImGui.Spacing();
-
-        DrawExecutorConfig();
-
-        ImGui.Separator();
-        ImGui.Spacing();
-
-        using (ImRaii.Disabled(PresetExecutor is { IsDisposed: false } || BetweenAreas))
+        using var tabBar = ImRaii.TabBar("###MainTabBar");
+        if (!tabBar) return;
+        
+        using (var mainPageItem = ImRaii.TabItem("主页"))
         {
-            if (ImGuiOm.ButtonSelectable("开始"))
+            if (mainPageItem)
             {
-                PresetExecutor?.Dispose();
-                PresetExecutor = null;
+                DrawExecutorInfo();
 
-                PresetExecutor ??= new(Service.Config.Presets[SelectedPresetIndex],
-                                       Service.Config.RunTimes);
+                ImGui.Separator();
+                ImGui.Spacing();
+
+                DrawExecutorConfig();
+
+                ImGui.Separator();
+                ImGui.Spacing();
+
+                using (ImRaii.Disabled(PresetExecutor is { IsDisposed: false } || BetweenAreas))
+                {
+                    if (ImGuiOm.ButtonSelectable("开始"))
+                    {
+                        PresetExecutor?.Dispose();
+                        PresetExecutor = null;
+
+                        PresetExecutor ??= new(Service.Config.Presets[SelectedPresetIndex],
+                                               Service.Config.RunTimes);
+                    }
+
+                    if (Service.Config.LeaderMode)
+                        ImGuiOm.TooltipHover("你已开启队长模式, 请阅读并确认下列注意事项:\n\n"  +
+                                             "1. 在任务搜索器内选取完成你所选择的副本\n"      +
+                                             "2. 配置好相关的任务搜索器设置 (如: 解除限制)\n" +
+                                             "3. 首次运作需要你手动排本, 后续为插件自动排本\n"  +
+                                             "4. 如果你目标副本是多变迷宫且为单人进本, 请先在任务搜索器内启用解除限制, 不然无法进本");
+                }
+
+                if (ImGuiOm.ButtonSelectable("结束"))
+                {
+                    PresetExecutor?.Dispose();
+                    PresetExecutor = null;
+                }
             }
-
-            if (Service.Config.LeaderMode)
-                ImGuiOm.TooltipHover("你已开启队长模式, 请阅读并确认下列注意事项:\n\n"  +
-                                     "1. 在任务搜索器内选取完成你所选择的副本\n"      +
-                                     "2. 配置好相关的任务搜索器设置 (如: 解除限制)\n" +
-                                     "3. 首次运作需要你手动排本, 后续为插件自动排本\n"  +
-                                     "4. 如果你目标副本是多变迷宫且为单人进本, 请先在任务搜索器内启用解除限制, 不然无法进本");
         }
 
-        if (ImGuiOm.ButtonSelectable("结束"))
+        using (var debugPageItem = ImRaii.TabItem("调试"))
         {
-            PresetExecutor?.Dispose();
-            PresetExecutor = null;
+            if (debugPageItem)
+            {
+                DrawDebugGeneralInfo();
+
+                ImGui.Separator();
+                ImGui.Spacing();
+
+                DrawDebugTargetInfo();
+                
+                ImGui.Separator();
+                ImGui.Spacing();
+
+                DrawDebugStatusInfo();
+            }
         }
     }
 
@@ -111,31 +133,9 @@ public class Main() : Window($"{PluginName} {Plugin.Version}###{PluginName}-Main
         ImGui.Text("运行信息:");
 
         ImGui.SameLine();
-        ImGui.Text($"{PresetExecutor?.RunningMessage ?? string.Empty}");
+        ImGui.TextWrapped($"{PresetExecutor?.RunningMessage ?? string.Empty}");
     }
-
-    private static void DrawNecessaryInfo()
-    {
-        ImGui.TextColored(LightBlue, "必要信息:");
-        using var indent = ImRaii.PushIndent();
-
-        ImGui.Text("当前区域:");
-
-        var zoneName = ZonePlaceNames.GetValueOrDefault(DService.ClientState.TerritoryType, "未知区域");
-        ImGui.SameLine();
-        ImGui.Text($"{zoneName} ({DService.ClientState.TerritoryType})");
-        ImGui.Text("当前目标:");
-
-        var target = DService.Targets.Target;
-        ImGui.SameLine();
-        ImGui.Text(target is not { ObjectKind:ObjectKind.BattleNpc } ? string.Empty : $"{target.Name} (DataID: {target.DataId})");
-
-        ImGui.Text("当前位置:");
-
-        ImGui.SameLine();
-        ImGui.Text($"{DService.ClientState.LocalPlayer?.Position:F2}");
-    }
-
+    
     private static void DrawExecutorConfig()
     {
         ImGui.TextColored(LightBlue, "运行设置:");
@@ -224,6 +224,135 @@ public class Main() : Window($"{PluginName} {Plugin.Version}###{PluginName}-Main
                                                groupSize with { X = ImGui.CalcTextSize("编辑预设").X * 1.5f }, true))
             WindowManager.Get<PresetEditor>().IsOpen ^= true;
     }
+    
+    private static void DrawDebugGeneralInfo()
+    {
+        ImGui.TextColored(LightBlue, "一般信息:");
+        using var indent = ImRaii.PushIndent();
+
+        using (ImRaii.Group())
+        {
+            var isCurrentZoneValid = LuminaGetter.TryGetRow<TerritoryType>(DService.ClientState.TerritoryType, out var zoneRow);
+            
+            ImGui.Text("当前区域:");
+            
+            var zoneName = LuminaWarpper.GetZonePlaceName(DService.ClientState.TerritoryType);
+            ImGui.SameLine();
+            ImGui.Text($"{zoneName} ({DService.ClientState.TerritoryType})");
+
+            if (isCurrentZoneValid)
+            {
+                ImGui.Text("副本区域:");
+
+                var contentName = LuminaWarpper.GetContentName(zoneRow.ContentFinderCondition.RowId);
+                ImGui.SameLine();
+                ImGui.Text($"{contentName} ({zoneRow.ContentFinderCondition.RowId})");
+                
+                ImGui.Text("副本用途:");
+                
+                ImGui.SameLine();
+                ImGui.Text($"{zoneRow.TerritoryIntendedUse.RowId}");
+            }
+        }
+
+        using (ImRaii.Group())
+        {
+            ImGui.Text("当前位置:");
+
+            ImGui.SameLine();
+            ImGui.Text($"{DService.ClientState.LocalPlayer?.Position:F2}");
+        }
+    }
+
+    private static void DrawDebugTargetInfo()
+    {
+        ImGui.TextColored(LightBlue, "目标信息:");
+        using var indent = ImRaii.PushIndent();
+        using var group = ImRaii.Group();
+
+        if (DService.Targets.Target is IBattleChara target)
+        {
+            ImGui.Text("当前目标:");
+
+            ImGui.SameLine();
+            ImGui.Text($"{target.Name} (0x{target.Address:X})");
+
+            ImGui.Text("目标类型:");
+
+            ImGui.SameLine();
+            ImGui.Text($"{target.ObjectKind} ({(byte)target.ObjectKind})");
+
+            ImGui.Text("Data ID:");
+
+            ImGui.SameLine();
+            ImGui.Text($"{target.DataId}");
+
+            ImGui.SameLine(0, 10f * ImGuiHelpers.GlobalScale);
+            ImGui.Text("Entity ID:");
+
+            ImGui.SameLine();
+            ImGui.Text($"{target.EntityId}");
+
+            ImGui.Text("目标位置:");
+
+            ImGui.SameLine();
+            ImGui.Text($"{target.Position:F2}");
+            
+            ImGui.Text("目标 HP:");
+            
+            ImGui.SameLine();
+            ImGui.Text($"{(double)target.CurrentHp / target.MaxHp * 100:F2}%% ({target.CurrentHp} / {target.MaxHp})");
+        }
+    }
+
+    private static void DrawDebugStatusInfo()
+    {
+        ImGui.TextColored(LightBlue, "状态效果信息:");
+        using var indent = ImRaii.PushIndent();
+        using var group  = ImRaii.Group();
+        
+        if (DService.ClientState.LocalPlayer is { } localPlayer)
+        {
+            using (ImRaii.Group())
+            {
+                ImGui.Text("自身");
+
+                foreach (var status in localPlayer.StatusList)
+                {
+                    if (!LuminaGetter.TryGetRow<Status>(status.StatusId, out var row)) continue;
+                    if (!DService.Texture.TryGetFromGameIcon(new(row.Icon), out var iconTexture)) continue;
+
+                    ImGui.Image(iconTexture.GetWrapOrEmpty().ImGuiHandle, ImGuiHelpers.ScaledVector2(24f));
+
+                    ImGui.SameLine();
+                    ImGui.AlignTextToFramePadding();
+                    ImGui.Text($"{row.Name} ({row.RowId})");
+                }
+            }
+        }
+        
+        if (DService.Targets.Target is IBattleChara target)
+        {
+            ImGui.SameLine();
+            using (ImRaii.Group())
+            {
+                ImGui.Text("目标");
+
+                foreach (var status in target.StatusList)
+                {
+                    if (!LuminaGetter.TryGetRow<Status>(status.StatusId, out var row)) continue;
+                    if (!DService.Texture.TryGetFromGameIcon(new(row.Icon), out var iconTexture)) continue;
+
+                    ImGui.Image(iconTexture.GetWrapOrEmpty().ImGuiHandle, ImGuiHelpers.ScaledVector2(24f));
+
+                    ImGui.SameLine();
+                    ImGui.AlignTextToFramePadding();
+                    ImGui.Text($"{row.Name} ({row.RowId})");
+                }
+            }
+        }
+    }
+
 
     public void Dispose()
     {
